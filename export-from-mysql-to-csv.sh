7052e180310b2528cdb541c3f6f88400fe8cacf0
Eric Bréchemier <medea@eric.brechemier.name>
2014-10-29 19:10:27 +0100

    Add script to export MySQL tables to CSV
    In the bootstrap phase, the source database is `giec`.
    It shall later be changed to `ipcc`.

---
+++ export-from-mysql-to-csv.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# Export data from MySQL database
+# to CSV files in data folder
+#
+# USAGE:
+# export-from-mysql-to-csv.sh [user] [host] [password]
+# with
+#   user - optional, string, database user name, defaults to 'root'
+#   host - optional, string, database host name, defaults to 'localhost'
+#   password - optional, string, database user password,
+#              defaults to 'no password' which provides no password;
+#              an empty string results in a prompt for password.
+user=${1:-'root'}
+host=${2:-'localhost'}
+password=${3:-'no password'}
+
+if [ "$password" = 'no password' ]
+then
+  passwordParam=''
+else
+  passwordParam="--password $password"
+fi
+
+# Use `giec` for bootstrap, to be replaced with `ipcc`
+database=giec
+query="mysql --host $host --user $user $passwordParam"
+
+table2csv()
+{
+  table=$(basename $1 .sql)
+  echo "Export table $database.$table to CSV file data/$table.csv"
+  $query $database < $1 \
+  | ../tsv2csv.sh > "../../data/$table.csv"
+}
+
+# change to the script's directory
+cd $(dirname $0)
+
+# change to import scripts directory
+cd scripts/import
+
+for script in *.sql
+do
+  table2csv "$script"
+done
+
+echo 'Export done.'
