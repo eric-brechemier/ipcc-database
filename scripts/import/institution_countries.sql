777ef012b4e5cf4901d77840305f6b0b4ba99bf2
Eric Bréchemier <medea@eric.brechemier.name>
2014-10-30 13:52:12 +0100

    Reorder institution countries by institution_id and country_id
    The goal is to find easily all the countries associated
    with a given institution.

--- scripts/import/institution_countries.sql
+++ scripts/import/institution_countries.sql
@@ -9,5 +9,7 @@ LEFT JOIN institutions
 ON institution_countries.institution_id = institutions.id
 LEFT JOIN countries
 ON institution_countries.country_id = countries.id
-ORDER BY institution_countries.id
+ORDER BY
+  institution_countries.institution_id,
+  institution_countries.country_id
 ;
