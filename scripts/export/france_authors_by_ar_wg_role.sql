7a7f8039ff483960bc94f664e84cf41de7bc0521
Eric Bréchemier <medea@eric.brechemier.name>
2014-11-20 11:13:12 +0100

    Add France Authors by AR, WG, Role (SQL and CSV)

---
+++ scripts/export/france_authors_by_ar_wg_role.sql
@@ -0,0 +1,38 @@
+-- Number of France authors by AR, WG and Role
+
+SELECT
+  roles.name AS `Role`,
+  ar AS AR,
+  wg AS WG,
+  france_authors.total AS `Total France Authors`,
+  all_authors.total AS `Total Authors`,
+  (
+    france_authors.total / all_authors.total
+  ) * 100 AS `Percentage of Authors in France` 
+FROM
+(
+  SELECT ar, wg, role, COUNT(*) AS total
+  FROM
+  (
+    SELECT DISTINCT ar, wg, role, author_id
+    FROM participations p
+    JOIN institution_countries ic ON p.institution_country_id = ic.id
+    JOIN countries c ON ic.country_id = c.id
+    WHERE c.name = 'France'
+  ) france_authors
+  GROUP BY ar, wg, role
+) france_authors
+JOIN
+(
+  SELECT ar, wg, role, COUNT(*) AS total
+  FROM
+  (
+    SELECT DISTINCT ar, wg, role, author_id
+    FROM participations
+  ) all_authors
+  GROUP BY ar, wg, role
+) all_authors
+USING (ar,wg,role)
+JOIN roles
+ON all_authors.role = roles.symbol
+ORDER BY roles.rank, ar, wg
