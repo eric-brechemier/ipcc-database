9e3a7fb2155e65e2b4940aa2ac986e8710ab4994
Eric Bréchemier <medea@eric.brechemier.name>
2014-11-20 10:37:06 +0100

    Add number of France participations by AR and WG (SQL and CSV)

---
+++ scripts/export/france_participations_by_ar_wg.sql
@@ -0,0 +1,26 @@
+-- Number of France Participations by AR and by WG
+
+SELECT
+  ar AS AR,
+  wg AS WG,
+  france_participations.total AS `Total France Participations`,
+  all_participations.total AS `Total Participations`,
+  (
+    france_participations.total / all_participations.total
+  ) * 100 AS `Percentage of France Participations`
+FROM
+(
+  SELECT ar, wg, COUNT(*) total
+  FROM participations p
+  JOIN institution_countries ic ON p.institution_country_id = ic.id
+  JOIN countries c ON ic.country_id = c.id
+  WHERE c.name = 'France'
+  GROUP BY ar, wg
+) france_participations
+JOIN
+(
+  SELECT ar, wg, COUNT(*) total
+  FROM participations
+  GROUP BY ar, wg
+) all_participations
+USING (ar,wg)
