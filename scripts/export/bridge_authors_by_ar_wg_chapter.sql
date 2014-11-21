358a3f31f5c703f73bda526328ae65050ec28db8
Eric Bréchemier <medea@eric.brechemier.name>
2014-11-21 14:23:31 +0100

    Add computation of working group participation ratios
    The subquery was copied from:
    ipcc-acquire/question03/compute-working-group-membership-ratios.sql
    
    It was renamed from `bridge_participations` to `bridge_wg_ratios`.

--- scripts/export/bridge_authors_by_ar_wg_chapter.sql
+++ scripts/export/bridge_authors_by_ar_wg_chapter.sql
@@ -27,6 +27,62 @@ JOIN
   AND p1.wg < p2.wg
 ) bridge_authors
 ON participations.author_id = bridge_authors.author_id
+JOIN
+(
+  SELECT
+    author_id,
+    total,
+    WG1 / total AS wg1,
+    WG2 / total AS wg2,
+    WG3 / total AS wg3,
+    2 * least(WG1, WG2) / total AS bridge12,
+    2 * least(WG2, WG3) / total AS bridge23,
+    2 * least(WG1, WG3) / total AS bridge13,
+    3 * least(WG1, WG2, WG3) / total AS bridge123
+  FROM
+    (
+      SELECT
+        total_participations.author_id,
+        IFNULL(WG1,0) AS WG1,
+        IFNULL(WG2,0) AS WG2,
+        IFNULL(WG3,0) AS WG3,
+        total
+      FROM
+        (
+          SELECT author_id, COUNT(id) as total
+          FROM participations
+          GROUP BY author_id
+        ) AS total_participations
+
+        LEFT JOIN
+        (
+          SELECT author_id, COUNT(id) AS WG1
+          FROM participations
+          WHERE wg = 1
+          GROUP BY author_id
+        ) AS wg1_participations
+        ON total_participations.author_id = wg1_participations.author_id
+
+        LEFT JOIN
+        (
+          SELECT author_id, COUNT(id) AS WG2
+          FROM participations
+          WHERE wg = 2
+          GROUP BY author_id
+        ) AS wg2_participations
+        ON total_participations.author_id = wg2_participations.author_id
+
+        LEFT JOIN
+        (
+          SELECT author_id, COUNT(id) AS WG3
+          FROM participations
+          WHERE wg = 3
+          GROUP BY author_id
+        ) AS wg3_participations
+        ON total_participations.author_id = wg3_participations.author_id
+    ) AS wg_participations
+) AS bridge_wg_ratios
+ON participations.author_id = bridge_wg_ratios.author_id
 JOIN authors
 ON participations.author_id = authors.id
 JOIN institution_countries
