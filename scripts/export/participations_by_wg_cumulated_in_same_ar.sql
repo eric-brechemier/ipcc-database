e45c00742b9eabcb0342be865d0cf6f1aff070a0
Eric Bréchemier <medea@eric.brechemier.name>
2014-11-24 17:39:29 +0100

    Add number of participations in cumulated WG within same AR

---
+++ scripts/export/participations_by_wg_cumulated_in_same_ar.sql
@@ -0,0 +1,24 @@
+-- Number of participations by AR and Cumulated WG
+
+SELECT
+  ar AS AR,
+  cumulated_wg AS `Cumulated WG`,
+  COUNT(*) AS `Total`
+FROM
+  (
+    SELECT
+      ar,
+      CONCAT(
+        'WG',
+        GROUP_CONCAT(
+          DISTINCT wg
+          ORDER BY wg
+          SEPARATOR '+'
+        )
+      ) cumulated_wg,
+      author_id
+    FROM participations
+    GROUP BY ar, author_id
+  ) author_participations
+GROUP BY ar, cumulated_wg
+ORDER BY ar, cumulated_wg
