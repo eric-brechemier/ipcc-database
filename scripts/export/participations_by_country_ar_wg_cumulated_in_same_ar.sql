6d84d029997611648fa55172e45774ff2403cb50
Eric Bréchemier <medea@eric.brechemier.name>
2014-11-25 15:32:37 +0100

    Add lines with zero authors for every country

--- scripts/export/participations_by_country_ar_wg_cumulated_in_same_ar.sql
+++ scripts/export/participations_by_country_ar_wg_cumulated_in_same_ar.sql
@@ -4,12 +4,35 @@ SELECT
   countries.name AS `Country`,
   all_participations.ar AS `AR`,
   all_participations.cumulated_wg `Cumulated WG`,
-  country_participations.total AS `Total Country Authors`,
+  IFNULL(country_participations.total,0) AS `Total Country Authors`,
   (
-    country_participations.total / all_participations.total
+    IFNULL(country_participations.total,0) / all_participations.total
   ) * 100 AS `% Country Authors`,
   all_participations.total AS `Total Authors`
 FROM
+countries
+JOIN
+(
+  SELECT ar, cumulated_wg, COUNT(*) AS total
+  FROM
+  (
+    SELECT
+      ar,
+      CONCAT(
+        'WG',
+        GROUP_CONCAT(
+          DISTINCT wg
+          ORDER BY wg
+          SEPARATOR '+'
+        )
+      ) cumulated_wg,
+      author_id
+    FROM participations
+    GROUP BY ar, author_id
+  ) all_participations
+  GROUP BY ar, cumulated_wg
+) all_participations
+LEFT JOIN
 (
   SELECT country_id, ar, cumulated_wg, COUNT(*) AS total
   FROM
@@ -33,31 +56,9 @@ FROM
   ) country_participations
   GROUP BY country_id, ar, cumulated_wg
 ) country_participations
-JOIN
-(
-  SELECT ar, cumulated_wg, COUNT(*) AS total
-  FROM
-  (
-    SELECT
-      ar,
-      CONCAT(
-        'WG',
-        GROUP_CONCAT(
-          DISTINCT wg
-          ORDER BY wg
-          SEPARATOR '+'
-        )
-      ) cumulated_wg,
-      author_id
-    FROM participations
-    GROUP BY ar, author_id
-  ) all_participations
-  GROUP BY ar, cumulated_wg
-) all_participations
-USING (ar, cumulated_wg)
-JOIN
-countries
-ON country_participations.country_id = countries.id
+ON countries.id = country_participations.country_id
+AND all_participations.ar = country_participations.ar
+AND all_participations.cumulated_wg = country_participations.cumulated_wg
 ORDER BY
   countries.name,
   all_participations.ar,
