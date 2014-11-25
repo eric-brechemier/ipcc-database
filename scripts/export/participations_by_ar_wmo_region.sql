6984b740a2ff1b4c82a8d0282b1b84c30b029501
Eric Bréchemier <medea@eric.brechemier.name>
2014-11-25 11:37:45 +0100

    Add percentage of participations of WMO Regions

--- scripts/export/participations_by_ar_wmo_region.sql
+++ scripts/export/participations_by_ar_wmo_region.sql
@@ -1,19 +1,38 @@
 -- Number of participations by AR and WMO Region
 
 SELECT
-  participations.ar AS `AR`,
-  groups.symbol AS `WMO Symbol`,
-  groups.name AS `WMO Group`,
-  COUNT(*) AS `Total`
-FROM participations
-JOIN institution_countries
-ON participations.institution_country_id = institution_countries.id
-JOIN countries
-ON institution_countries.country_id = countries.id
-JOIN groupings
-ON groupings.country_id = countries.id
-JOIN groups
-ON groupings.symbol = groups.symbol
-WHERE groups.type = 'WMO'
-GROUP BY participations.ar, groups.symbol
-ORDER BY participations.ar, `Total` DESC
+  all_participations.ar AS `AR`,
+  wmo_participations.symbol AS `WMO Symbol`,
+  wmo_participations.name AS `WMO Region`,
+  wmo_participations.total AS `Total WMO Region Participations`,
+  (
+    wmo_participations.total / all_participations.total
+  ) * 100 AS `% WMO Region Participations`,
+  all_participations.total AS `Total Participations`
+FROM
+(
+  SELECT
+    participations.ar,
+    groups.symbol,
+    groups.name,
+    COUNT(*) AS total
+  FROM participations
+  JOIN institution_countries
+  ON participations.institution_country_id = institution_countries.id
+  JOIN countries
+  ON institution_countries.country_id = countries.id
+  JOIN groupings
+  ON groupings.country_id = countries.id
+  JOIN groups
+  ON groupings.symbol = groups.symbol
+  WHERE groups.type = 'WMO'
+  GROUP BY participations.ar, groups.symbol
+) wmo_participations
+JOIN
+(
+  SELECT ar, COUNT(*) AS total
+  FROM participations
+  GROUP BY ar
+) all_participations
+USING (ar)
+ORDER BY wmo_participations.ar, wmo_participations.total DESC
